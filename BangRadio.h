5b82d6d7c0ed7a3a54d49e380f7f0113f7f2a203
John-Leitch <johnleitch@outlook.com>
2015-01-18 03:48:47 -0800

    Cleaned up whitespace

--- BangRadio.h
+++ BangRadio.h
@@ -24,14 +24,14 @@
 
 static inline int HashBuffer(int* buffer, int length)
 {
-	int hash = HashInit();
+    int hash = HashInit();
 
-	for (int j = 0; j < length; j++)
-	{
-		hash = HashRound(hash, buffer[j]);
-	}
+    for (int j = 0; j < length; j++)
+    {
+        hash = HashRound(hash, buffer[j]);
+    }
 
-	return HashFinalize(hash);
+    return HashFinalize(hash);
 }
 
 int i = 0;
@@ -49,60 +49,60 @@ int i = 0;
 #define SetRadioPin() ChangeRadioPin(output_high)
 #define ClearRadioPin() ChangeRadioPin(output_low)
 
-#define Send0()			\
-	SetRadioPin();		\
-	RadioDelay();		\
-	ClearRadioPin();	\
-	RadioDelay();		\
-	
-#define Send1()			\
-	ClearRadioPin();	\
-	RadioDelay();		\
-	SetRadioPin();		\
-	RadioDelay();		\
+#define Send0()        \
+    SetRadioPin();     \
+    RadioDelay();      \
+    ClearRadioPin();   \
+    RadioDelay();      \
+    
+#define Send1()        \
+    ClearRadioPin();   \
+    RadioDelay();      \
+    SetRadioPin();     \
+    RadioDelay();      \
 
-#define PulseRadioPin()	\
-SetRadioPin();			\
-RadioDelay();			\
-ClearRadioPin();		\
-RadioDelay();			\
+#define PulseRadioPin()    \
+SetRadioPin();             \
+RadioDelay();              \
+ClearRadioPin();           \
+RadioDelay();              \
 
 char tx8 = 0;
 int tx16 = 0;
 
-#define WriteBits(txBits, bitCount)	\
-	for (i = 0; i < bitCount; i++)	\
-	{                               \
-		if (txBits & 1)				\
-		{                           \
-			Send1();				\
-		}							\
-		else						\
-		{							\
-			Send0();				\
-		}							\
-									\
-		txBits >>= 1;				\
-	}								\
+#define WriteBits(txBits, bitCount)    \
+    for (i = 0; i < bitCount; i++)     \
+    {                                  \
+        if (txBits & 1)                \
+        {                              \
+            Send1();                   \
+        }                              \
+        else                           \
+        {                              \
+            Send0();                   \
+        }                              \
+                                       \
+        txBits >>= 1;                  \
+    }                                  \
 
 #define Write8() WriteBits(tx8, 8)
 #define Write16() WriteBits(tx16, 16)
 
 static inline void WriteBuffer(char* buffer, int length)
 {
-	int hash = HashInit();
-	
-	for (int j = 0; j < length; j++)
-	{
-		tx16 = MakeChunk(j + 1, buffer[j]);
-		hash = HashRound(hash, tx16);
-		Write16();
-		TransmitDelay();
-	}
-	
-	tx16 = HashFinalize(hash);
-	Write16();
-	TransmitDelay();
+    int hash = HashInit();
+    
+    for (int j = 0; j < length; j++)
+    {
+        tx16 = MakeChunk(j + 1, buffer[j]);
+        hash = HashRound(hash, tx16);
+        Write16();
+        TransmitDelay();
+    }
+    
+    tx16 = HashFinalize(hash);
+    Write16();
+    TransmitDelay();
 }
 
 #define ChunkSize 2
@@ -127,77 +127,77 @@ int rx16 = 0;
 #define ReceiverPin 8
 #define GetReceiverBit() PINB & 1
 
-#define ReadBits(rxBits, bitCount)	\
-	rxBits = 0;						\
-									\
-	for (i = 0; i < bitCount; i++)	\
-	{								\
-		bit0 = GetReceiverBit();	\
-		RadioDelay();				\
-		bit1 = GetReceiverBit();	\
-		RadioDelay();				\
-									\
-		if (bit0 == bit1)			\
-		{							\
-			rxBits = -1;			\
-			break;					\
-		}							\
-									\
-		rxBits |= (bit1 << i);		\
-	}								\
-									\
+#define ReadBits(rxBits, bitCount)  \
+    rxBits = 0;                     \
+                                    \
+    for (i = 0; i < bitCount; i++)  \
+    {                               \
+        bit0 = GetReceiverBit();    \
+        RadioDelay();               \
+        bit1 = GetReceiverBit();    \
+        RadioDelay();               \
+                                    \
+        if (bit0 == bit1)           \
+        {                           \
+            rxBits = -1;            \
+            break;                  \
+        }                           \
+                                    \
+        rxBits |= (bit1 << i);      \
+    }                               \
+                                    \
 
 #define Read8() ReadBits(rx8, 8)
 #define Read16() ReadBits(rx16, 16)
 
 static inline bool ReadBuffer(int* packet, int length)
 {
-	char* chunks = (char*)malloc(length);
-	memset(chunks, 0, length);	
-	short currentChunk = 0;
-	bool waiting = true;
-	int j = 0;
-	
-	while (waiting)
-	{
-		Read16();
+    char* chunks = (char*)malloc(length);
+    memset(chunks, 0, length);    
+    short currentChunk = 0;
+    bool waiting = true;
+    int j = 0;
+    
+    while (waiting)
+    {
+        Read16();
 
-		if (rx16 != -1)
-		{
-			if (rx16 & HashBit())
-			{
-				currentChunk = length - 1;
-			}
-			else
-			{
-				currentChunk = (rx16 >> 8) - 1;
+        if (rx16 != -1)
+        {
+            if (rx16 & HashBit())
+            {
+                currentChunk = length - 1;
+            }
+            else
+            {
+                currentChunk = (rx16 >> 8) - 1;
 
-				if (currentChunk > length - 2)
-				{
-					continue;
-				}
-			}
-			
-			packet[currentChunk] = rx16;
-			chunks[currentChunk] = 1;
-			waiting = false;
+                if (currentChunk > length - 2)
+                {
+                    continue;
+                }
+            }
+            
+            packet[currentChunk] = rx16;
+            chunks[currentChunk] = 1;
+            waiting = false;
 
-			for (j = 0; j < length; j++)
-			{
-				if (!chunks[j])
-				{
-					waiting = true;
-					break;
-				}
-			}
-		}
-	}
-	
-	currentChunk = length - 1;
-	int hash = HashBuffer(packet, currentChunk);	
-	free(chunks);
-	
-	return hash == packet[currentChunk];
+            for (j = 0; j < length; j++)
+            {
+                if (!chunks[j])
+                {
+                    waiting = true;
+                    break;
+                }
+            }
+        }
+    }
+    
+    currentChunk = length - 1;
+    int hash = HashBuffer(packet, currentChunk);    
+    free(chunks);
+    
+    return hash == packet[currentChunk];
 }
 
 #endif
